6f7a9b57fdc02aaf646fc829faf0e8edb0cf1386
kakahuy113 <kakahuy113@gmail.com>
2020-07-03 21:39:46 +0700

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,96 @@ import {
 	Loading
 } from './util/utilities';
 import Cookie from './lib/Cookie';
-
 document.addEventListener('DOMContentLoaded', () => {
 	Cookie();
 	getSVGs();
 	Loading();
 });
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 5,
+		spaceBetween: 30,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+	  });
+	var swiper = new Swiper('.swiper-container_brand', {
+		spaceBetween: 30,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+		
+	  });
+	  var swiper = new Swiper('.swiper-container_home', {
+		spaceBetween: 30,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+		
+	  })
+	  
+	  var swiper = new Swiper('.swiper-container_banner', {
+		spaceBetween: 30,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+		
+	  })
+	  var swiper = new Swiper('.swiper-container_brand', {
+		slidesPerView: 5,
+		spaceBetween: 15,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+	  });
 
+	//   var swiper = new Swiper('.slider_customer', {
+	// 	slidesPerView: 5,
+	// 	spaceBetween: 15,
+	// 	pagination: {
+	// 	  el: '.swiper-pagination',
+	// 	  clickable: true,
+	// 	},
+	//   });
+	  var swiper = new Swiper('.slider_product_detail', {
+		slidesPerView: 5,
+		spaceBetween: 5,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+	  });
+	  
+	  var swiper = new Swiper('.slider_sptt', {
+		slidesPerView: 5,
+		spaceBetween: 5,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+	  });
+	  $('.visible_parameter').click((event) => {
+		  event.preventDefault();
+		  $('.visible_info').removeClass("active_bg")
+		  $('.visible_parameter').addClass("active_bg")
+		  $("div.inner_product_info").css("display", "none");
+		  $("div.product_detail_num").css("display", "block");
+		  
+	  })
+	  $('.visible_info').click((event) => {
+		event.preventDefault();
+		$('.visible_parameter').removeClass("active_bg")
+		$('.visible_info').addClass("active_bg")
+		$("div.product_detail_num").css("display", "none");
+		$("div.inner_product_info").css("display", "block");
+	})
+});
 
 
 // CHECK FORM VALID
